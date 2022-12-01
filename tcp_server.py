7ba54a1a775796b1e293d261561faeef27184650
Ari5400 <94087682+ArielElbaz@users.noreply.github.com>
2022-12-01 20:00:28 +0200

    s.

--- tcp_server.py
+++ tcp_server.py
@@ -26,24 +26,30 @@ def close(list):
         if l == "Connection: keep-alive":
             return "keep-alive"
 
-
+def getPath(fileName):
+    if fileName.startswith("files"):
+        return fileName
+    if fileName == '':
+        return fileName + 'files/index.html'
+    else:
+        return 'files/' + fileName
 
 while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
     data = client_socket.recv(1000)
     fileLines = data.decode('utf-8').splitlines(True)
-    filePath = fileName(fileLines[0])
+    filePath = getPath(fileName(fileLines[0]))
     ext = os.path.splitext(filePath)[-1].lower()
     connStatus = close(fileLines)
 
-    if filePath == 'GET /redirect HTTP/1.1':
+    if filePath == '/redirect':
         data = 'HTTP/1.1 301 Moved Permanently\nConnection: close\nLocation: result.html\n\n'
         client_socket.send(data.encode())
         client_socket.send(''.encode())
 
-    if filePath == '/':
-        filePath = 'files/index.html'
+    # if filePath == '/':
+    #     filePath = 'files/index.html'
     if path.exists(filePath):
         # open images as binary file.
         if ext == '.jpg' or ext == '.ico' or ext == '.png':
@@ -56,18 +62,16 @@ while True:
         else:
             with open(filePath, 'r', encoding='utf-8') as file:
                 fileContent = file.read()
-                temp = 'HTTP/1.1 200 OK\nConnection: {conn}\nContent-Length:{length} \n\n'.format(conn = connStatus, length = os.path.getsize(filePath))
-                data = temp.encode('utf-8') + fileContent.encode('utf-8')
-                finalData = data.encode('utf-8')
-                # data = 'HTTP/1.1 200 OK\nConnection: {conn}\nContent-Length:{length}\n\n{fileContent}'.format(
-                #     conn=connStatus,
-                #     length=os.path.getsize(filePath), fileContent=fileContent)
-                client_socket.send(finalData)
+                data = 'HTTP/1.1 200 OK\nConnection: {conn}\nContent-Length:{length}\n\n {fileContent}'.format(
+                    conn=connStatus,
+                    length=os.path.getsize(filePath), fileContent=fileContent)
+                client_socket.send(data.encode('utf-8'))
     # file isn't found - send 404 error.
     else:
         data = 'HTTP/1.1 404 Not Found\nConnection: {conn}\n\n'.format(
             conn="close")
         client_socket.send(data.encode())
+        client_socket.send(''.encode())
 
     if connStatus == "close":
         client_socket.close()
